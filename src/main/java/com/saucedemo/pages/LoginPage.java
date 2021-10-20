5a0926f3746d0bb1618b4740cf8452a25918ccb6
jose carlos franco <josecarlos_jc007@hotmail.com>
2021-10-20 08:27:40 -0300

    Implementa mapeamento de novos elementos e seus métodos da página de LoginPage.java

--- src/main/java/com/saucedemo/pages/LoginPage.java
+++ src/main/java/com/saucedemo/pages/LoginPage.java
@@ -10,6 +10,11 @@ public class LoginPage {
     private By usernameField = By.id("user-name");
     private By passwordField = By.id("password");
     private By loginButton = By.id("login-button");
+    private By logoImagem = By.xpath("//*[@id=\"root\"]/div/div[1]");
+    private By botImagem = By.xpath("//*[@id=\"root\"]/div/div[2]/div[1]/div[2]");
+    private By mensageerrorContainer = By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]");
+    private By credentialsLogin = By.id("login_credentials");
+    private By passwordLogin = By.id("login_password");
     private WebDriver driver;
 
     //Construtor
@@ -26,6 +31,22 @@ public class LoginPage {
     }
     public WebElement getLoginButton(){
         return this.driver.findElement(loginButton);
+    }
+
+    public WebElement getLogoImagem(){
+        return this.driver.findElement(logoImagem);
+    }
+    public WebElement getBotImagem(){
+        return this.driver.findElement(botImagem);
+    }
+    public WebElement getMensageerrorContainer(){
+        return this.driver.findElement(mensageerrorContainer);
+    }
+    public WebElement getCredentialsLogin(){
+        return this.driver.findElement(credentialsLogin);
+    }
+    public WebElement getPasswordLogin() {
+        return this.driver.findElement(passwordLogin);
     }
 }
 
